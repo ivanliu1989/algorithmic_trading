ccad3172447ad71b762cd0613a6d71de5ba0501d
Ivan Liu <Ivan Liu>
2016-09-14 23:30:38 +1000

    first mean reversion model

--- examples/chapter2_2.R
+++ examples/chapter2_2.R
@@ -7,12 +7,22 @@ library(PerformanceAnalytics)
 library(fUnitRoots)
 library(FGN)
 
-getFX("AUD/USD",
-      from = Sys.Date() - 365*2,
-      to = Sys.Date())
-plot(AUDUSD)
-
+getSymbols("EWA")
+getSymbols("EWC")
+plot(EWA)
+plot(EWC)
 
 # 1. CADF Test for Cointegration ------------------------------------------
-
-
+EWA <-EWA$EWA.Adjusted
+index(EWA) <- as.Date(index(EWA))
+EWC <-EWC$EWC.Adjusted
+index(EWC) <- as.Date(index(EWC))
+Pairs <- merge(EWA, EWC)
+colnames(Pairs) <- c("EWA", "EWC")
+# Time-series
+chart.TimeSeries(Pairs, ylog=TRUE,cex.legend=1.25,
+                          colorset=c("cadetblue","darkolivegreen3"))
+# Correlation
+chart.Correlation(Pairs, ylog=TRUE,cex.legend=1.25,
+                  colorset=c("cadetblue","darkolivegreen3"))
+# Residuals of Linear Regression
