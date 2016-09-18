068cf3c8c2d2ed3ce08f6bff1b1e4e101fb3a250
Ivan Liu <Ivan Liu>
2016-09-18 23:50:16 +1000

    Monte Carlo Simulation

--- 2.MCMC Simulations/monteCarloSimulation.R
+++ 2.MCMC Simulations/monteCarloSimulation.R
@@ -12,7 +12,9 @@ source("1.Kalman Filter/kalmanFilter.R")
 # 1. Load data and calculate correlations ---------------------------------
 getFX("AUD/USD")
 getFX("CAD/USD")
+y = CADUSD
 
 # 2. Moments --------------------------------------------------------------
-
-
+ret <- ROC(y)[-1]
+moments = c(mean(ret), sd(ret), skewness(ret), kurtosis(ret))
+ret_sim = rpearson(100000, moments = moments)
